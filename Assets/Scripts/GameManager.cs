5989ef1e05308e9bf57850c7fd9aefd95bdf6c82
willthom1080 <102179711+willthom1080@users.noreply.github.com>
2023-02-06 01:15:37 -0500

    Updated GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Tilemaps;
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     GameObject[] rootContainer;
+    public RuleTile[] tilesContainer;
+    public Tilemap rootTileMap;
     public bool victory;
     void Start()
     {
        rootContainer = GameObject.FindGameObjectsWithTag("Root");
+       int i = 0;
+       foreach( GameObject x in rootContainer)
+        {
+            Root target = x.GetComponent<Root>();
+            target.rootTiles = rootTileMap;
+            target.newRoot = tilesContainer[i];
+            target.player = GameObject.FindGameObjectWithTag("Player");
+            i++;
+        }
     }
 
     // Update is called once per frame
